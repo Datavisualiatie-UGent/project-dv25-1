c4a7ecbeae37d22e17c509bd0b45dacde5677458
RunoBoy <77899156+RunoBoy@users.noreply.github.com>
2025-04-27 22:34:08 +0000

    Deploying to gh-pages from @ Datavisualiatie-UGent/project-dv25-1@c659057ab069088128003e5603c8139f039b5cc3 🚀

--- _import/components/example.5dfb32b9.js
+++ _import/components/example.5dfb32b9.js
@@ -108,4 +108,67 @@ export function Importance_Slider() {
     return Inputs.range([0, 5], {label: "How important is workout out for you?", step: .1})
 }
 
-// export function
+export function Moment() {
+    return Inputs.radio(["Early morning", "Afternoon", "Evening"], {
+        label: "When do you workout?",
+        value: "Afternoon"
+    })
+}
+
+export function Length() {
+    return Inputs.radio(["I don't really exercise", "30 minutes", "1 hour", "2 hours", "3 hours and above"], {
+        label: "How long do you workout?",
+        value: "30 minutes"
+    })
+}
+
+export function Healthy_Slider() {
+    return Inputs.range([0, 5], {label: "How healthy do you feel?", step: 1})
+}
+
+
+export function Healthy(data, moment, length, healthy_slider) {
+    let skip = "I don't really exercise";
+
+    let data_processed = data.map(row => ({
+        moment: row.exercise_time,
+        length: row.exercise_length,
+        healthy: row.how_healthy
+    }));
+
+    const userInput = {
+        moment: moment,
+        length: length,
+        healthy: healthy_slider
+    }
+
+    const moment_order = ["Early morning", "Afternoon", "Evening"];
+    const length_order = [skip, "30 minutes", "1 hour", "2 hours", "3 hours and above"];
+
+    return Plot.plot({
+        marginLeft: 120,
+        padding: 0,
+        x: {domain: moment_order},
+        y: {domain: length_order},
+        color: {legend: true, zero: true},
+        marks: [
+            Plot.cell(
+                data_processed,
+                Plot.group(
+                    {fill: "median"},
+                    {x: d => d.moment, y: d => d.length, fill: d => d.healthy, inset: 0.5}
+                )
+            ),
+            Plot.dot(
+                [userInput],
+                {
+                    x: d => d.moment,
+                    y: d => d.length,
+                    stroke: "black",  // Outline the dot to make it pop
+                    fill: d => d.healthy, // Same color scale
+                    r: 8 // radius of the dot
+                }
+            )
+        ]
+    });
+}
